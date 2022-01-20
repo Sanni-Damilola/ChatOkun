02e94b37d8dc1e90eaec0a341a35f10d5c32b581
Damilola Sanni <sannifortune11@gmail.com>
2022-01-20 11:02:23 +0100

    start

--- frontend/src/note.ts
+++ frontend/src/note.ts
@@ -2,3 +2,4 @@
 this code is not displaying on my Browser And I don Know Why
 this code is not displaying on my Browser And I don Know Why
 this code is not displaying on my Browser And I don Know Why
+this code is not displaying on my Browser And I don Know Why
